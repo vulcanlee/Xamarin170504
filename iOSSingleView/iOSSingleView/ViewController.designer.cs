fed166a918999d9ad9af2b0f6841ee5365b0b410
Vulcan <vulcan.lee@gmail.com>
2017-05-04 11:28:18 +0800

    Xamarin.iOS

---
+++ iOSSingleView/iOSSingleView/ViewController.designer.cs
@@ -0,0 +1,46 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace iOSSingleView
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton fooButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel fooLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField fooTextField { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (fooButton != null) {
+                fooButton.Dispose ();
+                fooButton = null;
+            }
+
+            if (fooLabel != null) {
+                fooLabel.Dispose ();
+                fooLabel = null;
+            }
+
+            if (fooTextField != null) {
+                fooTextField.Dispose ();
+                fooTextField = null;
+            }
+        }
+    }
+}
