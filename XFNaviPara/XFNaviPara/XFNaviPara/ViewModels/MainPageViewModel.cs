dece774ce2a19485cdc61ffb7b68650dd7d7b7be
Vulcan <vulcan.lee@gmail.com>
2017-05-04 16:13:28 +0800

    頁面導航與參數傳遞

---
+++ XFNaviPara/XFNaviPara/XFNaviPara/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,62 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XFNaviPara.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        #region MyEntry
+        private string _MyEntry;
+        /// <summary>
+        /// PropertyDescription
+        /// </summary>
+        public string MyEntry
+        {
+            get { return this._MyEntry; }
+            set { this.SetProperty(ref this._MyEntry, value); }
+        }
+        #endregion
+
+
+        public DelegateCommand NextPageCommand { get; set; }
+
+        private readonly INavigationService _navigationService;
+        public MainPageViewModel(INavigationService navigationService)
+        {
+
+            _navigationService = navigationService;
+            NextPageCommand = new DelegateCommand(async () =>
+            {
+
+               await _navigationService.NavigateAsync("NextPage?Message="+ MyEntry);
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
---
+++ XFNaviPara/XFNaviPara/XFNaviPara/ViewModels/NextPageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XFNaviPara.ViewModels
+{
+    public class NextPageViewModel : BindableBase, INavigationAware
+    {
+
+        #region MyLabel
+        private string _MyLabel;
+        /// <summary>
+        /// PropertyDescription
+        /// </summary>
+        public string MyLabel
+        {
+            get { return this._MyLabel; }
+            set { this.SetProperty(ref this._MyLabel, value); }
+        }
+        #endregion
+
+        public NextPageViewModel()
+        {
+
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("Message"))
+            {
+                var fooMessage = parameters["Message"] as string;
+                MyLabel = "Hello " + fooMessage;
+            }
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+        }
+    }
+}
