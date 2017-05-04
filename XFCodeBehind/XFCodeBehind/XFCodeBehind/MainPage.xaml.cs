a6e36c2e4be4be44374a75a4838a817bb3d36c27
Vulcan <vulcan.lee@gmail.com>
2017-05-04 12:35:33 +0800

    Xamarin.Forms Code Behind

---
+++ XFCodeBehind/XFCodeBehind/XFCodeBehind/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFCodeBehind
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            fooButton.Clicked += ButtonClick;
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            fooLabel.Text = $"Hello {fooEntry.Text}";
+        }
+    }
+}
